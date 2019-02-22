6d6176359dbb537762e27d948238837bf1d585e5
Matthias Carnein <Matthias.Carnein@uni-muenster.de>
2019-02-22 06:33:56 +0100

    added file option dialogue, improve random forest learning

--- moa/src/main/java/moa/clusterers/meta/EnsembleClustererAbstract.java
+++ moa/src/main/java/moa/clusterers/meta/EnsembleClustererAbstract.java
@@ -1,7 +1,8 @@
 package moa.clusterers.meta;
 
-import java.util.ArrayList;
 
+import java.util.ArrayList;
+import com.github.javacliparser.FileOption;
 import com.github.javacliparser.IntOption;
 import com.github.javacliparser.ListOption;
 import com.github.javacliparser.Option;
@@ -9,20 +10,19 @@ import com.yahoo.labs.samoa.instances.Attribute;
 import com.yahoo.labs.samoa.instances.DenseInstance;
 import com.yahoo.labs.samoa.instances.Instance;
 import com.yahoo.labs.samoa.instances.Instances;
-import com.yahoo.labs.samoa.instances.InstancesHeader;
-
 import moa.classifiers.meta.AdaptiveRandomForestRegressor;
 import moa.cluster.Clustering;
 import moa.clusterers.AbstractClusterer;
 import moa.clusterers.Clusterer;
+import moa.clusterers.denstream.WithDBSCAN;
 import moa.core.Measurement;
 import moa.core.ObjectRepository;
 import moa.evaluation.SilhouetteCoefficient;
 import moa.gui.visualization.DataPoint;
 import moa.options.ClassOption;
-import moa.streams.InstanceStream;
 import moa.streams.clustering.RandomRBFGeneratorEvents;
 import moa.tasks.TaskMonitor;
+
 
 public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 
@@ -33,19 +33,27 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 	int bestModel;
 	ArrayList<DataPoint> windowPoints;
 	AdaptiveRandomForestRegressor[] ARFregs;
-		
+	
+	
 	public ListOption baselearnersOption = new ListOption("baseClusterer", 'b',
 			"The clusterers the ensemble consists of.",
-			new ClassOption("learner", ' ', "", Clusterer.class, "clustree.ClusTree"),
+			new ClassOption("learner", ' ', "", Clusterer.class, "denstream.WithDBSCAN"),
 			new Option[] {
-					new ClassOption("", ' ', "", Clusterer.class, "clustree.ClusTree"),
-					new ClassOption("", ' ', "", Clusterer.class, "clustream.WithKmeans") },
+					new ClassOption("", ' ', "", Clusterer.class, "denstream.WithDBSCAN -e 0.06"),
+					new ClassOption("", ' ', "", Clusterer.class, "denstream.WithDBSCAN -e 0.05") },
 			',');
 	
 	public IntOption windowSizeOption = new IntOption("windowSize", 'w',
 			"The window size over which Online Performance Estimation is done.", 1000,
 			1, Integer.MAX_VALUE);
 
+	public FileOption fileOption = new FileOption("ConfigurationFile", 'f', "Configuration file in json format.",
+            "", "json", false);
+	
+	
+    public void init() {
+    	this.fileOption.getFile();
+    }
 	
 	@Override
 	public boolean isRandomizable() {
@@ -92,6 +100,7 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 		if (this.instancesSeen % windowSizeOption.getValue() == 0) {
 			updateConfiguration();	
 			windowPoints.clear(); // flush the current window
+			System.out.println("--------------");
 		}
 		
 		this.instancesSeen++;		
@@ -109,9 +118,10 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 			// evaluate clustering if we have one
 			// if no algorithm produces a clustering, the old clusterer will remain active			
 			if(result != null) {
-				silh.evaluateClustering(result, null, windowPoints); // compute evaluation measure, true clustering not needed for silouette
+				// compute silhouette width
+				silh.evaluateClustering(result, null, windowPoints);
 				double performance = silh.getLastValue(0);
-				System.out.println(performance);
+				System.out.println("Result -e=" + ((WithDBSCAN)this.ensemble[i]).epsilonOption.getValue() + ":\t" + performance);
 				
 				// find best clustering result
 				if(performance > maxVal){
@@ -120,26 +130,89 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 				}
 				
 				
-				// train adaptive random forest based on performance of model
-				double[] values = {1,2,3, performance}; // create new instance based on settings and performance
+//				Option[] learnerOptions = this.baselearnersOption.getList();
+//				System.out.println(this.ensemble[1].getOptions()); // get all available options
+//				System.out.println(learnerOptions[0].getValueAsCLIString());
+//				System.out.println(learnerOptions[0].getStateString());
+//				System.out.println(learnerOptions[0].getDefaultCLIString());
+//				System.out.println(learnerOptions[0].getCLIChar());
+				
+				// unpack CLI string into params and class
+//				String cliString = this.ensemble[0].getCLICreationString(Clusterer.class);
+//
+//		        cliString = cliString.trim();
+//		        int firstSpaceIndex = cliString.indexOf(' ', 0);
+//		        String className;
+//		        ArrayList<String> classOptions = new ArrayList<String>();
+//		        ArrayList<String> classOptionValues = new ArrayList<String>();
+//
+//		        if (firstSpaceIndex > 0) {
+//		            className = cliString.substring(0, firstSpaceIndex); // get class name
+//		            int index = cliString.indexOf("-", firstSpaceIndex+1);
+//		            while (index >= 0) {
+//		                int nextIndex = cliString.indexOf("-", index + 1);
+//		                if(nextIndex < 0) {
+//			                String test = cliString.substring(index+1, cliString.length()).trim());
+//		                } else {
+//			                classOptions.add(cliString.substring(index+1, nextIndex).trim());
+//		                }
+//		                index = nextIndex;
+//		            }
+//		        } else {
+//		            className = cliString;
+//		        }
+
+				
+				
+//				String cliString = this.ensemble[0].getCLICreationString(Clusterer.class);
+//
+//		        cliString = cliString.trim();
+//		        int firstSpaceIndex = cliString.indexOf(' ', 0);
+//		        String className;
+//		        if (firstSpaceIndex > 0) {
+//			        cliString = cliString.substring(firstSpaceIndex, cliString.length()).trim();
+//
+//		        	String[] options = cliString.split("-");
+//		        	for(int j =0; j < options.length; j++) {
+//			        	String[] option = cliString.trim().split(" ");
+//			        	String opt = option[0];
+//			        	String val = option[1];
+//		        	}
+//		        } else {
+//		            className = cliString;
+//		        }
+//				
+						
+				// create new instance based on settings and performance
+				double[] values = {((WithDBSCAN)this.ensemble[i]).epsilonOption.getValue(), performance};
 		        Instance inst = new DenseInstance(1.0, values);
 		        
-		        // add header
+		        // parameter settings are the attributes
 		        ArrayList<Attribute> attributes = new ArrayList<Attribute>();
-		        for (int j = 0; j < 3; j++) {
+		        for (int j = 0; j < 1; j++) {
 		            attributes.add(new Attribute("att" + (j + 1)));
 		        }
-		        
-		        // performance measure is the "class"
-		        ArrayList<String> classLabels = new ArrayList<String>();
-		        for (int j = 0; j < 1; j++) {
-		            classLabels.add("class" + (j + 1));
-		        }	        
-		        
+	            attributes.add(new Attribute("class1"));
+		        	
+				// add header
 		        Instances dataset = new Instances(null,	attributes, 0);
+		        dataset.setClassIndex(dataset.numAttributes() - 1);
+
 		        inst.setDataset(dataset);
 
+				// train adaptive random forest regressor based on performance of model
 				this.ARFregs[i].trainOnInstanceImpl(inst);
+				
+				
+				// predict performance of new configuration
+				double[] vals = {0.02};
+				Instance newInst = new DenseInstance(1.0, vals);
+		        Instances noClassDataset = new Instances(null, attributes, 0);
+		        noClassDataset.setClassIndex(noClassDataset.numAttributes() - 1); // TODO why is this even necessary, class does not exist
+		        newInst.setDataset(noClassDataset);
+		        
+				double prediction = this.ARFregs[i].getVotesForInstance(newInst)[0];
+				System.out.println("-> Prediction -e=" +  vals[0] + ":\t"+ prediction);
 			}
 		}
 	}
@@ -195,7 +268,7 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 			Instance inst = stream.nextInstance().getData();
 			algorithm.trainOnInstanceImpl(inst);
 		}
-		Clustering cluster = algorithm.getClusteringResult();
+		algorithm.getClusteringResult();
 		
 		System.out.println("-------------");
 		
@@ -207,7 +280,7 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 			Instance inst = stream2.nextInstance().getData();
 			algorithm2.trainOnInstanceImpl(inst);
 		}
-		Clustering cluster2 = algorithm2.getClusteringResult();
+		algorithm2.getClusteringResult();
 		
 	}
 	
