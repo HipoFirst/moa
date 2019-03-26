942402a91c0c87ba6716894022fb38cf35f9b973
Matthias Carnein <Matthias.Carnein@uni-muenster.de>
2019-03-26 10:44:49 +0100

    fixed class problems, replace existing configurations

--- moa/src/main/java/moa/clusterers/meta/EnsembleClustererAbstract.java
+++ moa/src/main/java/moa/clusterers/meta/EnsembleClustererAbstract.java
@@ -4,9 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import com.github.javacliparser.FileOption;
+import com.github.javacliparser.FloatOption;
+import com.github.javacliparser.IntOption;
 import com.github.javacliparser.ListOption;
 import com.github.javacliparser.Option;
+import com.github.javacliparser.Options;
 import com.google.gson.Gson;
 import com.yahoo.labs.samoa.instances.Attribute;
 import com.yahoo.labs.samoa.instances.DenseInstance;
@@ -38,7 +43,7 @@ class AlgorithmSettings {
 
 class GeneralSettings {
 	public int windowSize;
-	public AlgorithmSettings[] algorithms;
+	public AlgorithmSettings[] algorithms; // TODO we could make this an arraylist and fill more solutions in
 }
 
 public abstract class EnsembleClustererAbstract extends AbstractClusterer {
@@ -50,7 +55,7 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 	int bestModel;
 	ArrayList<DataPoint> windowPoints;
 	AdaptiveRandomForestRegressor ARFreg;
-	int windowSize;
+	GeneralSettings settings;
 
 	public FileOption fileOption = new FileOption("ConfigurationFile", 'f', "Configuration file in json format.",
 			"settings.json", ".json", false);
@@ -80,7 +85,7 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 
 		this.instancesSeen = 0;
 		this.bestModel = 0;
-		this.windowPoints = new ArrayList<DataPoint>(this.windowSize);
+		this.windowPoints = new ArrayList<DataPoint>(this.settings.windowSize);
 
 		this.ARFreg = new AdaptiveRandomForestRegressor(); // create regressor
 		this.ARFreg.prepareForUse();
@@ -94,6 +99,7 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 	public void trainOnInstanceImpl(Instance inst) {
 		DataPoint point = new DataPoint(inst, instancesSeen); // create data points from instance
 		this.windowPoints.add(point); // remember points of the current window
+		this.instancesSeen++;
 
 		// train all models
 		for (int i = 0; i < this.ensemble.length; i++) {
@@ -101,73 +107,121 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 		}
 
 		// every windowSize we update the configurations
-		if (this.instancesSeen % this.windowSize == 0) {
+		if (this.instancesSeen % this.settings.windowSize == 0) {
+			System.out.println("\n-------------- Processed " + instancesSeen + " Instances --------------");
 			updateConfiguration();
 			windowPoints.clear(); // flush the current window
-			System.out.println("--------------");
 		}
-
-		this.instancesSeen++;
 	}
 
 	protected void updateConfiguration() {
 		// init evaluation measure
 		SilhouetteCoefficient silh = new SilhouetteCoefficient();
 
-		// parameter settings are the attributes, class label is the performance
+		// parameter settings are the attributes, class label is the performance // TODO init for every algorithm separately?
 		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
-		for (int j = 0; j < 1; j++) {
+		for (int j = 0; j < this.settings.algorithms[0].parameters.length; j++) {
 			attributes.add(new Attribute("att" + (j + 1)));
 		}
-		attributes.add(new Attribute("class1"));
+//		attributes.add(new Attribute("class1"));
 
 		double maxVal = -1 * Double.MAX_VALUE;
 		for (int i = 0; i < this.ensemble.length; i++) {
 			// get current macro clusters
 			Clustering result = this.ensemble[i].getClusteringResult();
 
-			// evaluate clustering if we have one
-			// if no algorithm produces a clustering, the old clusterer will remain active
-			if (result != null) {
-				// compute silhouette width
-				silh.evaluateClustering(result, null, windowPoints);
-				double performance = silh.getLastValue(0);
-				System.out.println(
-						"Result " + this.ensemble[i].getCLICreationString(Clusterer.class) + ":\t" + performance);
+			// evaluate clustering using silhouette width
+			silh.evaluateClustering(result, null, windowPoints);
+			double performance = silh.getLastValue(0);
+			System.out.println("Result " + this.ensemble[i].getCLICreationString(Clusterer.class) + ":\t" + performance);
 //				System.out.println(ClassOption.stripPackagePrefix(this.ensemble[i].getClass().getName(), Clusterer.class)); // print class
 //				System.out.println(this.ensemble[i].getOptions().getAsCLIString()); // print non-default options
 
-				// find best clustering result
-				if (performance > maxVal) {
-					maxVal = performance;
-					this.bestModel = i; // the clusterer with the best result becomes the active one
-				}
-
-				// create new instance based on settings and performance to train regressor
-				double[] values = { ((WithDBSCAN) this.ensemble[i]).epsilonOption.getValue(), performance };
-				Instance inst = new DenseInstance(1.0, values);
-
-				// add header to dataset
-				Instances dataset = new Instances(null, attributes, 0);
-				dataset.setClassIndex(dataset.numAttributes() - 1);
-
-				inst.setDataset(dataset);
-
-				// train adaptive random forest regressor based on performance of model
-				this.ARFreg.trainOnInstanceImpl(inst);
+			// find best clustering result
+			if (performance > maxVal) {
+				maxVal = performance;
+				this.bestModel = i; // the clusterer with the best result becomes the active one
 			}
-		}
+			
+			// create new instance based on settings and performance to train regressor
+			double[] params = new double[this.settings.algorithms[i].parameters.length+1];
+			for(int j=0; j<this.settings.algorithms[i].parameters.length; j++) {
+				params[j] = this.settings.algorithms[i].parameters[j].value;
+			}
+			params[params.length-1] = performance;
+			Instance inst = new DenseInstance(1.0, params);
 
+			// add header to dataset
+			Instances dataset = new Instances(null, attributes, 0);
+			dataset.setClassIndex(dataset.numAttributes());
+
+			inst.setDataset(dataset);
+
+			// train adaptive random forest regressor based on performance of model
+			this.ARFreg.trainOnInstanceImpl(inst);
+		}
+		System.out.println("Clusterer " + this.bestModel + " is the active clusterer.");
+
+
+		tryNewConfiguration(silh, attributes);
+	}
+	
+	protected void tryNewConfiguration(SilhouetteCoefficient silh, ArrayList<Attribute> attributes) {
 		// predict performance of new configuration
-		double[] vals = { 0.07, 0 }; // TODO adding a dummy class for now because it crashes otherwise
+		double[] vals = { 0.07, 0.3}; // TODO adding a dummy class for now because it crashes otherwise
 		Instance newInst = new DenseInstance(1.0, vals);
 		Instances newDataset = new Instances(null, attributes, 0);
-		newDataset.setClassIndex(newDataset.numAttributes() - 1);
+		newDataset.setClassIndex(newDataset.numAttributes());
 		newInst.setDataset(newDataset);
 
 		double prediction = this.ARFreg.getVotesForInstance(newInst)[0];
-		System.out.println("-> Prediction -e=" + vals[0] + ":\t" + prediction);
+		System.out.println("-> Prediction " + Arrays.toString(vals) + ":\t" + prediction);
+		
+		// if we found a promising solution
+		if(prediction > silh.getMinValue(0)){
+			ArrayList<Double> silhs = silh.getAllValues(0);
+			// replace by sampling proportionally
+			int replaceIdx = sampleProportionally(silhs);
+			System.out.println("New Configuration appears to be promising! Replace configuration: " + replaceIdx);
+			
+			// store new config in settings
+			for(int j=0; j<vals.length-1; j++) {
+				this.settings.algorithms[replaceIdx].parameters[j].value = vals[j];
+			}
+			
+			// construct CLI string
+			StringBuilder commandLine = new StringBuilder();
+			commandLine.append(this.settings.algorithms[replaceIdx].algorithm); // add algorithm class
+			for (ParameterSettings option : this.settings.algorithms[replaceIdx].parameters) {
+				commandLine.append(" ");
+				commandLine.append("-" + option.parameter); // as well as all parameters
+				commandLine.append(" " + option.value);
+			}
+			System.out.println("Initialise: " + commandLine.toString());
+
+			// create new clusterer from CLI string
+			ClassOption opt = new ClassOption("", ' ', "", Clusterer.class, commandLine.toString());
+			this.ensemble[replaceIdx] = (Clusterer) opt.materializeObject(null, null);
+			this.ensemble[replaceIdx].prepareForUse();
+		}
 	}
+	
+	protected int sampleProportionally(ArrayList<Double> values) {
+        double completeWeight = 0.0;
+        for (Double value : values)
+            completeWeight += value;
+        
+        double r = Math.random() * completeWeight;
+        double countWeight = 0.0;
+        for (int j=0; j<values.size(); j++) {
+            countWeight += values.get(j);
+            if (countWeight >= r)
+                return j;
+        }
+        throw new RuntimeException("Sampling failed");
+    }
+	
+	
 
 	@Override
 	protected Measurement[] getModelMeasurementsImpl() {
@@ -186,18 +240,16 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileOption.getValue()));
 			Gson gson = new Gson();
-			GeneralSettings settings = gson.fromJson(bufferedReader, GeneralSettings.class);
+			this.settings = gson.fromJson(bufferedReader, GeneralSettings.class);
 
-			this.windowSize = settings.windowSize;
-
-			this.ensemble = new Clusterer[settings.algorithms.length];
+			this.ensemble = new Clusterer[this.settings.algorithms.length];
 			// for all algorithms
-			for (int i = 0; i < settings.algorithms.length; i++) {
+			for (int i = 0; i < this.settings.algorithms.length; i++) {
 
 				// construct CLI string
 				StringBuilder commandLine = new StringBuilder();
-				commandLine.append(settings.algorithms[i].algorithm); // add algorithm class
-				for (ParameterSettings option : settings.algorithms[i].parameters) {
+				commandLine.append(this.settings.algorithms[i].algorithm); // add algorithm class
+				for (ParameterSettings option : this.settings.algorithms[i].parameters) {
 					commandLine.append(" ");
 					commandLine.append("-" + option.parameter); // as well as all parameters
 					commandLine.append(" " + option.value);
@@ -208,6 +260,7 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 				ClassOption opt = new ClassOption("", ' ', "", Clusterer.class, commandLine.toString());
 				this.ensemble[i] = (Clusterer) opt.materializeObject(monitor, repository);
 			}
+			
 		} catch (
 
 		FileNotFoundException e) {
