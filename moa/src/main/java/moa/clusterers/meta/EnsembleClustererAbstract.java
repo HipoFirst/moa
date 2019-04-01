26bb79603f3d6ce13774096f9e3653c154cf16bd
Matthias Carnein <Matthias.Carnein@uni-muenster.de>
2019-04-01 17:05:11 +0200

    improved support for nominal values

--- moa/src/main/java/moa/clusterers/meta/EnsembleClustererAbstract.java
+++ moa/src/main/java/moa/clusterers/meta/EnsembleClustererAbstract.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import com.github.javacliparser.FileOption;
 import com.google.gson.Gson;
 import com.yahoo.labs.samoa.instances.Attribute;
@@ -27,46 +28,119 @@ import moa.tasks.TaskMonitor;
 // This class contains the individual parameter settings (such as limits and current value)
 class ParameterConfiguration {
 	public String parameter;
-	public double value;
-	public double min;
-	public double max;
+	public Object value;
+	public Object[] range;
 	public String type;
 }
 
-class NumericParameterConfiguration extends ParameterConfiguration{
-	public double std;
-
-	public NumericParameterConfiguration(ParameterConfiguration x) {
-		this.parameter = x.parameter;
-		this.value = x.value;
-		this.min = x.min;
-		this.max = x.max;
-		this.type = x.type;
-		this.std = (this.max - this.min) / 2;
-	}
-}
-
-
-// This class contains the settings of an algorithm (such as name and the actual
-// clusterer object) as well as an array of Parameter Settings
+// This class contains the settings of an algorithm (such as name) as well as an
+// array of Parameter Settings
 class AlgorithmConfiguration {
 	public String algorithm;
 	public ParameterConfiguration[] parameters;
-	public Clusterer clusterer;
+}
 
-	public AlgorithmConfiguration(AlgorithmConfiguration x) {
+// This contains the general settings (such as the max ensemble size) as well as
+// an array of Algorithm Settings
+class GeneralConfiguration {
+	public int windowSize;
+	public int ensembleSize;
+	public int newConfigurations;
+	public AlgorithmConfiguration[] algorithms;
+}
+
+
+
+
+
+class NumericalParameter {
+	public String parameter;
+	public double value;
+	public double[] range;
+	public double std;
+	public Attribute attribute;
+	
+
+	public NumericalParameter(ParameterConfiguration x) {
+		this.parameter = x.parameter;
+		this.value = (double) x.value;
+		this.range = new double[x.range.length];
+		for (int i = 0; i < x.range.length; i++){
+			range[i] = (double) x.range[i];
+		}
+		this.std = (this.range[1] - this.range[0]) / 2;
+		this.attribute = new Attribute(x.parameter);
+	}
+}
+
+class NominalParameter {
+	public String parameter;
+	public String value;
+	public String[] range;
+	public Attribute attribute;
+
+	public NominalParameter(ParameterConfiguration x) {
+		this.parameter = x.parameter;
+		this.value = String.valueOf(x.value);
+		this.range = new String[x.range.length];
+		for (int i = 0; i < x.range.length; i++){
+			range[i] = String.valueOf(x.range[i]);
+		}
+		this.attribute = new Attribute(x.parameter, Arrays.asList(range));
+
+	}
+}
+
+class Algorithm {
+	public String algorithm;
+	public ArrayList<NumericalParameter> numericalParameters;
+	public ArrayList<NominalParameter> nominalParameters;
+	public Clusterer clusterer;
+	public ArrayList<Attribute> attributes;
+
+	public Algorithm(Algorithm x) {
 
 		this.algorithm = x.algorithm;
-		this.parameters = new ParameterConfiguration[x.parameters.length];
-		for (int i = 0; i < x.parameters.length; i++) {
-			this.parameters[i] = new NumericParameterConfiguration(x.parameters[i]);
-		}
+		// TODO this is probably referencing
+		// should be a deep copy but doesnt matter since the algorithm not reinitialised
+		this.numericalParameters = x.numericalParameters; 
+		this.nominalParameters = x.nominalParameters;
+		this.attributes = x.attributes;
 
+		// init(); // we dont initialise here because we want to manipulate the parameters first
+	}
+
+	public Algorithm(AlgorithmConfiguration x) {
+
+		this.algorithm = x.algorithm;
+		this.numericalParameters = new ArrayList<NumericalParameter>();
+		this.nominalParameters = new ArrayList<NominalParameter>();
+		this.attributes = new ArrayList<Attribute>();
+		for (ParameterConfiguration param : x.parameters) {
+			if (param.type.equals("numeric")) {
+				NumericalParameter numParam = new NumericalParameter(param);
+				this.numericalParameters.add(numParam);
+				this.attributes.add(new Attribute(numParam.parameter));
+			} else if (param.type.equals("categorical")) {
+				NominalParameter nomParam = new NominalParameter(param);
+				this.nominalParameters.add(nomParam);
+				this.attributes.add(new Attribute(nomParam.parameter, Arrays.asList(nomParam.range)));
+			}
+		}
+		init();
+	}
+
+	public void init(){
 		// initialise a new algorithm using the Command Line Interface (CLI)
 		// construct CLI string from settings, e.g. denstream.WithDBSCAN -e 0.08 -b 0.3
 		StringBuilder commandLine = new StringBuilder();
 		commandLine.append(this.algorithm); // first the algorithm class
-		for (ParameterConfiguration option : this.parameters) {
+		for (NumericalParameter option : this.numericalParameters) {
+			commandLine.append(" ");
+			commandLine.append("-" + option.parameter); // then the parameter
+			commandLine.append(" " + option.value); // and its value
+		}
+		for (NominalParameter option : this.nominalParameters) {
 			commandLine.append(" ");
 			commandLine.append("-" + option.parameter); // then the parameter
 			commandLine.append(" " + option.value); // and its value
@@ -80,15 +154,6 @@ class AlgorithmConfiguration {
 	}
 }
 
-// This contains the general settings (such as the max ensemble size) as well as
-// an array of Algorithm Settings
-class GeneralSettings {
-	public int windowSize;
-	public int ensembleSize;
-	public int newConfigurations;
-	public AlgorithmConfiguration[] algorithms;
-}
-
 public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 
 	private static final long serialVersionUID = 1L;
@@ -96,10 +161,10 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 	int instancesSeen;
 	int currentEnsembleSize;
 	int bestModel;
-	ArrayList<AlgorithmConfiguration> ensemble;
+	ArrayList<Algorithm> ensemble;
 	ArrayList<DataPoint> windowPoints;
 	AdaptiveRandomForestRegressor ARFreg;
-	GeneralSettings settings;
+	GeneralConfiguration settings;
 
 	// the file option dialogue in the UI
 	public FileOption fileOption = new FileOption("ConfigurationFile", 'f', "Configuration file in json format.",
@@ -164,27 +229,19 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 	protected void updateConfiguration() {
 		// init evaluation measure (silhouette for now)
 		SilhouetteCoefficient silh = new SilhouetteCoefficient();
-
-		// parameter settings are the attributes, class label is the performance // TODO
-		// init for every algorithm separately?
-		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
-		for (int j = 0; j < this.settings.algorithms[0].parameters.length; j++) {
-			attributes.add(new Attribute("att" + (j + 1)));
-		}
-		// attributes.add(new Attribute("class1"));
-
 		// train the random forest regressor based on the configuration performance
 		// and find the best performing algorithm
-		evaluatePerformance(silh, attributes);
+		evaluatePerformance(silh);
 
 		System.out.println("Clusterer " + this.bestModel + " is the active clusterer.");
 
 		// generate a new configuration and predict its performance using the random
 		// forest regressor
-		predictConfiguration(silh, attributes);
+		predictConfiguration(silh);
 	}
 
-	protected void evaluatePerformance(SilhouetteCoefficient silh, ArrayList<Attribute> attributes) {
+	protected void evaluatePerformance(SilhouetteCoefficient silh) {
+		
 		double maxVal = -1 * Double.MAX_VALUE;
 		for (int i = 0; i < this.ensemble.size(); i++) {
 			// get macro clusters of this clusterer
@@ -209,16 +266,17 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 			// create new training instance based to train regressor
 			// features are the algorithm and its configuration, the class is its
 			// performance in the last window
-			double[] params = new double[this.ensemble.get(i).parameters.length + 1];
-			for (int j = 0; j < this.ensemble.get(i).parameters.length; j++) {
-				params[j] = this.ensemble.get(i).parameters[j].value; // add configuration as features
+			double[] params = new double[this.ensemble.get(i).numericalParameters.size() + 1];
+			int pos = 0;
+			for (NumericalParameter param : this.ensemble.get(i).numericalParameters) {
+				params[pos++] = param.value; // add configuration as features
 			}
+
 			params[params.length - 1] = performance; // add performance as class
 			Instance inst = new DenseInstance(1.0, params);
 
-			// add header to dataset (same as before) TODO: do we need an attribute for the
-			// class label?
-			Instances dataset = new Instances(null, attributes, 0);
+			// add header to dataset TODO: do we need an attribute for the class label?
+			Instances dataset = new Instances(null, this.ensemble.get(i).attributes, 0);
 			dataset.setClassIndex(dataset.numAttributes()); // set class index to our performance feature
 			inst.setDataset(dataset);
 
@@ -228,7 +286,7 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 	}
 
 	// predict performance of new configuration
-	protected void predictConfiguration(SilhouetteCoefficient silh, ArrayList<Attribute> attributes) {
+	protected void predictConfiguration(SilhouetteCoefficient silh) {
 
 		// irace sampling of new configurations:
 		// Integer and ordinal: round(rtnorm(1, mean + 0.5, stdDev, lowerBound,
@@ -244,7 +302,6 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 		// sample a parent configuration proportionally to its performance from the
 		// ensemble
 		ArrayList<Double> silhs = silh.getAllValues(0);
-		// ArrayList<AlgorithmConfiguration> newConfigs = new ArrayList<AlgorithmConfiguration>[>this.settings.newConfigurations];
 
 		for (int z = 0; z < this.settings.newConfigurations; z++) {
 			int parentIdx = sampleProportionally(silhs);
@@ -252,52 +309,37 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 					+ this.ensemble.get(parentIdx).clusterer.getCLICreationString(Clusterer.class));
 
 			// sample new configuration from the parent
-			double[] vals = new double[this.ensemble.get(parentIdx).parameters.length];
-			for (int i = 0; i < this.ensemble.get(parentIdx).parameters.length; i++) {
-
+			double[] vals = new double[this.ensemble.get(parentIdx).numericalParameters.size()];
+			int pos = 0;
+			for (NumericalParameter param : this.ensemble.get(parentIdx).numericalParameters) {
 				// for numeric features use truncated normal distribution
-				if (this.ensemble.get(parentIdx).parameters[i] instanceof NumericParameterConfiguration) {
-					NumericParameterConfiguration paramConfig = (NumericParameterConfiguration) this.ensemble.get(parentIdx).parameters[i];
-
-					double mean = paramConfig.value;
-					double std = paramConfig.std;
-					double lb = paramConfig.min;
-					double ub = paramConfig.max;
-					TruncatedNormal trncnormal = new TruncatedNormal(mean, std, lb, ub);
-					vals[i] = trncnormal.sample();
-					System.out.println("Sample new configuration for parameter -"
-							+ this.ensemble.get(parentIdx).parameters[i].parameter + " with mean: " + mean + ", std: "
-							+ std + ", lb: " + lb + ", ub: " + ub + "\t=>\t -" + this.ensemble.get(parentIdx).parameters[i].parameter + " " + vals[i]);
-				} else {
-					throw new RuntimeException("Only numeric features implemented so far.");
-				}
+				double mean = param.value;
+				double std = param.std;
+				double lb = param.range[0];
+				double ub = param.range[1];
+				TruncatedNormal trncnormal = new TruncatedNormal(mean, std, lb, ub);
+				vals[pos++] = trncnormal.sample();
+				System.out.println("Sample new configuration for parameter -" + param.parameter + " with mean: " + mean
+						+ ", std: " + std + ", lb: " + lb + ", ub: " + ub + "\t=>\t -" + param.parameter + " "
+						+ vals[pos - 1]);
 			}
 
 
-			// for (int i = 0; i < this.ensemble.size(); i++) {
-			// 	for (int j = 0; j < this.ensemble.get(i).parameters.length; j++) {
-			// 		if (this.ensemble.get(i).parameters[j].type.equals("numeric")) {
-			// 			this.ensemble.get(i).parameters[j].std = this.ensemble.get(i).parameters[j].std * ((1 / this.settings.newConfigurations)^(1 / this.ensemble.get(i).parameters.length));
-			// 		}
-			// 	}
-			// }
-
-
 			Instance newInst = new DenseInstance(1.0, vals);
-			Instances newDataset = new Instances(null, attributes, 0);
+			Instances newDataset = new Instances(null, this.ensemble.get(parentIdx).attributes, 0);
 			newDataset.setClassIndex(newDataset.numAttributes());
 			newInst.setDataset(newDataset);
 
 			double prediction = this.ARFreg.getVotesForInstance(newInst)[0];
 			System.out.print("Predict " + ClassOption
 					.stripPackagePrefix(this.ensemble.get(parentIdx).clusterer.getClass().getName(), Clusterer.class));
-			for (int i = 0; i < this.ensemble.get(parentIdx).parameters.length; i++) {
-				System.out.print(" -" + this.ensemble.get(parentIdx).parameters[i].parameter + " " + vals[i]);
+			for (int i = 0; i < this.ensemble.get(parentIdx).numericalParameters.size(); i++) {
+				System.out.print(" -" + this.ensemble.get(parentIdx).numericalParameters.get(i).parameter + " " + vals[i]);
 			}
 			System.out.println("\t => \t Silhouette: " + prediction);
 
-			//TODO if ensemble empty, we could also just fill
-			if(Double.isNaN(prediction)){
+			// TODO if ensemble empty, we could also just fill
+			if (Double.isNaN(prediction)) {
 				return;
 			}
 
@@ -306,22 +348,22 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 				System.out.println("Ensemble not full. Add configuration as new algorithm.");
 
 				// copy existing clusterer configuration but change settings
-				AlgorithmConfiguration newAlgorithmConfig = new AlgorithmConfiguration(this.ensemble.get(parentIdx));
+				Algorithm newAlgorithm = new Algorithm(this.ensemble.get(parentIdx));
 				for (int i = 0; i < vals.length; i++) {
-					newAlgorithmConfig.parameters[i].value = vals[i];
-					
+					NumericalParameter newParam = newAlgorithm.numericalParameters.get(i);
+					newParam.value = vals[i];
+
 					// Reduce standard deviation for next iteration
 					// TODO this is not directly transferable from irace due to different algorithms
-					if (newAlgorithmConfig.parameters[i] instanceof NumericParameterConfiguration) {
-						NumericParameterConfiguration paramConfig = (NumericParameterConfiguration) newAlgorithmConfig.parameters[i];
-						paramConfig.std = paramConfig.std * ((1 / this.settings.newConfigurations)^(1 / newAlgorithmConfig.parameters.length));
-					}
+					newParam.std = newParam.std * (Math.pow((1.0 / this.settings.newConfigurations), (1.0 / newAlgorithm.numericalParameters.size())));
 				}
+				newAlgorithm.init();
+
 				// add to ensemble
-				this.ensemble.add(newAlgorithmConfig);
+				this.ensemble.add(newAlgorithm);
 
 				// update current silhouettes with the prediction
-				silhs.add(prediction); 
+				silhs.add(prediction);
 
 			} else if (prediction > silh.getMinValue(0)) {
 				// if the predicted performance is better than the one we have in the ensemble
@@ -332,12 +374,14 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 						"Ensemble already full but new configuration is promising! Replace algorithm: " + replaceIdx);
 
 				// copy existing clusterer configuration but change settings
-				AlgorithmConfiguration newConfig = new AlgorithmConfiguration(this.ensemble.get(parentIdx));
+				Algorithm newAlgorithm = new Algorithm(this.ensemble.get(parentIdx));
 				for (int i = 0; i < vals.length; i++) {
-					newConfig.parameters[i].value = vals[i];
+					newAlgorithm.numericalParameters.get(i).value = vals[i];
 				}
+				newAlgorithm.init();
+
 				// replace in ensemble
-				this.ensemble.set(replaceIdx, newConfig);
+				this.ensemble.set(replaceIdx, newAlgorithm);
 
 				// update current silhouettes with the prediction
 				silhs.set(replaceIdx, prediction);
@@ -380,14 +424,14 @@ public abstract class EnsembleClustererAbstract extends AbstractClusterer {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileOption.getValue()));
 			Gson gson = new Gson();
 			// store settings in dedicated class structure
-			this.settings = gson.fromJson(bufferedReader, GeneralSettings.class);
+			this.settings = gson.fromJson(bufferedReader, GeneralConfiguration.class);
 
 			// also create the ensemble which can be larger than the provided (starting)
 			// configurations
-			this.ensemble = new ArrayList<AlgorithmConfiguration>(this.settings.ensembleSize);
+			this.ensemble = new ArrayList<Algorithm>(this.settings.ensembleSize);
 			// copy and initialise the provided starting configurations in the ensemble
 			for (int i = 0; i < this.settings.algorithms.length; i++) {
-				this.ensemble.add(new AlgorithmConfiguration(this.settings.algorithms[i]));
+				this.ensemble.add(new Algorithm(this.settings.algorithms[i]));
 			}
 
 		} catch (
